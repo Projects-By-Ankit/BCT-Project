2a4995337811cf76cfc7b88bf889c82285b29412
Projects-By-Ankit <86493360+Projects-By-Ankit@users.noreply.github.com>
2022-11-20 13:35:12 +0530

    Initital commit

---
+++ Network/Network.py
@@ -0,0 +1,15 @@
+class Network:
+    def __init__(self):
+        self.Queue = []
+
+    def add(self, block):
+        pass
+
+    def remove(self, block):
+        pass
+
+    def process(self, block):
+        pass
+
+    def operate(self):
+        pass
